49438b04a4472d1eec23620c53b215d2ef45fa49
Cica Olympio <cicaolym@gmail.com>
2021-11-10 01:35:49 -1100

    Update ratings.js

--- js/ratings.js
+++ js/ratings.js
@@ -4,3 +4,13 @@ function collect_ratings(){
 }
 
 const elements=document.querySelectorAll(".rating");
+elements.foreach(element => {
+rating=parseInt(element.id.replace('star', '');
+ratings.count+=parseInt(element.value);
+ratings.sum+=parseInt(element.value)*rating
+});
+if(ratings.count!=0)
+{
+  ratings.average=ratings.sum/ratings.count
+}
+return ratings
